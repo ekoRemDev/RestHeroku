5d59129169e939f85c4b5d8650fe8810fcf4054f
Eko <ekoremdev@gmail.com>
2020-07-27 13:39:34 +0400

    getMapping Added

--- src/main/java/uk/co/flyingpigstudios/controller/ApiController.java
+++ src/main/java/uk/co/flyingpigstudios/controller/ApiController.java
@@ -1,10 +1,12 @@
 package uk.co.flyingpigstudios.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ApiController {
 
+    @GetMapping("/")
     public String index(){
         return "Rest Heroku Works...";
     }
